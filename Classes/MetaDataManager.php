fd689fb8eeec119b058c43aec075ebd34d91659b
Daniel Lienert <daniel@lienert.cc>
2016-05-28 09:00:15 +0200

    TASK: Add extracted tags to asset model

--- Classes/MetaDataManager.php
+++ Classes/MetaDataManager.php
@@ -12,10 +12,12 @@ namespace Neos\MetaData;
  * source code.
  */
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Neos\MetaData\Domain\Collection\MetaDataCollection;
 use TYPO3\Media\Domain\Model\Asset;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Eel\Utility as EelUtility;
+use TYPO3\Media\Domain\Model\Tag;
 
 
 class MetaDataManager
@@ -28,6 +30,12 @@ class MetaDataManager
     protected $eelEvaluator;
 
     /**
+     * The default context variables available inside Eel
+     * @var array
+     */
+    protected $defaultContextVariables;
+
+    /**
      * @Flow\Inject
      * @var \TYPO3\Media\Domain\Repository\AssetRepository
      */
@@ -37,8 +45,19 @@ class MetaDataManager
      * @Flow\InjectConfiguration(package="Neos.MetaData", path="metaDataMapping")
      * @var array
      */
-    protected $settings;
+    protected $metaDataMappingConfiguration;
 
+    /**
+     * @Flow\InjectConfiguration(package="Neos.MetaData", path="defaultEelContext")
+     * @var array
+     */
+    protected $defaultEelContext;
+
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\Media\Domain\Repository\TagRepository
+     */
+    protected $tagRepository;
 
     /**
      * @param Asset $asset
@@ -46,6 +65,10 @@ class MetaDataManager
      */
     public function updateMetaDataForAsset(Asset $asset, MetaDataCollection $metaDataCollection)
     {
+        if ($this->defaultContextVariables === NULL) {
+            $this->defaultContextVariables = EelUtility::getDefaultContextVariables($this->defaultEelContext);
+        }
+
         $this->mapMetaDataToAsset($asset, $metaDataCollection);
         $this->emitMetaDataCollectionUpdated($asset, $metaDataCollection);
     }
@@ -58,15 +81,43 @@ class MetaDataManager
      */
     protected function mapMetaDataToAsset(Asset $asset, MetaDataCollection $metaDataCollection)
     {
-        if (isset($this->settings['title'])) {
-            $asset->setTitle(EelUtility::evaluateEelExpression($this->settings['title'], $this->eelEvaluator, $metaDataCollection->toArray()));
+        $contextVariables = array_merge($this->defaultContextVariables, $metaDataCollection->toArray());
+
+        if (isset($this->metaDataMappingConfiguration['title'])) {
+            $asset->setTitle(EelUtility::evaluateEelExpression($this->metaDataMappingConfiguration['title'], $this->eelEvaluator, $contextVariables));
         }
 
-        if (isset($this->settings['caption'])) {
-            $asset->setCaption(EelUtility::evaluateEelExpression($this->settings['caption'], $this->eelEvaluator, $metaDataCollection->toArray()));
+        if (isset($this->metaDataMappingConfiguration['caption'])) {
+            $asset->setCaption(EelUtility::evaluateEelExpression($this->metaDataMappingConfiguration['caption'], $this->eelEvaluator, $contextVariables));
         }
 
+        if (isset($this->metaDataMappingConfiguration['tags'])) {
+            $tagLabels = EelUtility::evaluateEelExpression($this->metaDataMappingConfiguration['tags'], $this->eelEvaluator, $contextVariables);
+
+            $tags = new ArrayCollection();
+            foreach ($tagLabels as $tagLabel) {
+                $tags->add($this->getOrCreateTag($tagLabel));
+            }
+            $asset->setTags($tags);
+        }
+        
         $this->assetRepository->update($asset);
+    }
+
+
+    /**
+     * @param string $label
+     * @return Tag
+     */
+    protected function getOrCreateTag($label) {
+        $tag = $this->tagRepository->findByLabel($label);
+
+        if(!($tag instanceof Tag)) {
+            $tag = new Tag($label);
+            $this->tagRepository->add($tag);
+        }
+
+        return $tag;
     }
 
     /**
