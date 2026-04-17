c4e076a38df34d5c542729d9a1236b6af2923bfb
Michel Loew <michel.loew@jvm.ch>
2026-04-17 11:09:33 +0200

    FIX: Adapt MigrationCommandController to new Reference interface

--- Classes/Command/AssetMetaDataMigrationCommandController.php
+++ Classes/Command/AssetMetaDataMigrationCommandController.php
@@ -7,8 +7,9 @@ namespace Neos\MetaData\Command;
 use Neos\Flow\Cli\CommandController;
 use Neos\Media\Domain\Model\Asset;
 use Neos\Media\Domain\Repository\AssetRepository;
+use Neos\MetaData\ConfigurationProvider\ArrayMetaDataConfigurationProvider;
+use Neos\MetaData\Domain\Dto\MetaDataAssetReference;
 use Neos\MetaData\Domain\Dto\MetaDataPropertyName;
-use Neos\MetaData\Domain\Dto\MetaDataPropertyValue;
 use Neos\MetaData\MetaDataManager;
 
 final class AssetMetaDataMigrationCommandController extends CommandController
@@ -16,6 +17,7 @@ final class AssetMetaDataMigrationCommandController extends CommandController
 
     public function __construct(
         private readonly MetaDataManager $metaDataManager,
+        private readonly ArrayMetaDataConfigurationProvider $metaDataConfigurationProvider,
         private readonly AssetRepository $assetRepository,
     )
     {
@@ -23,31 +25,41 @@ final class AssetMetaDataMigrationCommandController extends CommandController
     }
     public function migrateExistingAssetPropertiesCommand(): void
     {
+        $metaDataDimensionSpacePoints = $this->metaDataConfigurationProvider->getConfiguration()->dimensions;
         foreach ($this->assetRepository->findAll() as $asset) {
             /** @var Asset $asset */
             $title = $asset->getTitle();
             $caption = $asset->getCaption();
             $copyrightNotice = $asset->getCopyrightNotice();
-            if (!empty($title)) {
-                $this->metaDataManager->setMetaDataPropertyValue(
-                    $asset->getIdentifier(),
-                    MetaDataPropertyName::fromString('title'),
-                    MetaDataPropertyValue::parse($asset->getTitle() ?? ''),
-                );
-            }
-            if (!empty($caption)) {
-                $this->metaDataManager->setMetaDataPropertyValue(
-                    $asset->getIdentifier(),
-                    MetaDataPropertyName::fromString('caption'),
-                    MetaDataPropertyValue::parse($asset->getCaption() ?? ''),
-                );
-            }
-            if (!empty($copyrightNotice)) {
-                $this->metaDataManager->setMetaDataPropertyValue(
-                    $asset->getIdentifier(),
-                    MetaDataPropertyName::fromString('copyrightNotice'),
-                    MetaDataPropertyValue::parse($asset->getCopyrightNotice() ?? ''),
-                );
+            $metaDataAssetReference = new MetaDataAssetReference(
+                $asset->getAssetSourceIdentifier(),
+                $asset->getIdentifier(),
+            );
+            foreach ($metaDataDimensionSpacePoints as $dimensionSpacePoint) {
+                if (!empty($title)) {
+                    $this->metaDataManager->setMetaDataPropertyValue(
+                        $metaDataAssetReference,
+                        MetaDataPropertyName::fromString('title'),
+                        $asset->getTitle(),
+                        $dimensionSpacePoint,
+                    );
+                }
+                if (!empty($caption)) {
+                    $this->metaDataManager->setMetaDataPropertyValue(
+                        $metaDataAssetReference,
+                        MetaDataPropertyName::fromString('caption'),
+                        $asset->getCaption(),
+                        $dimensionSpacePoint,
+                    );
+                }
+                if (!empty($copyrightNotice)) {
+                    $this->metaDataManager->setMetaDataPropertyValue(
+                        $metaDataAssetReference,
+                        MetaDataPropertyName::fromString('copyrightNotice'),
+                        $asset->getCopyrightNotice(),
+                        $dimensionSpacePoint,
+                    );
+                }
             }
         }
     }
